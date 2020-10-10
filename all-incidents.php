31ce5d7719b8c1af908c0296615dffb3afdc6af2
naimcm@gmail.com <naimcm@gmail.com>
2020-10-11 00:18:33 +0600

    Commit for 11-oct-2020
    Edit form modal add
    
    view delete done also

--- all-incidents.php
+++ all-incidents.php
@@ -40,8 +40,8 @@
                                     <td><?= $row['Status'] ?></td>
                                     <td>
                                         <a href="javascript:avoid(0)" data-toggle="modal" data-target="#Ref_ID-<?= $row['Ref_ID'] ?>"<i class="fa fa-eye"></i>&nbsp;</a>
-                                        <a href=""><i class="fa fa-pencil"></i>&nbsp;</a>
-                                        <a href="delete.php?DeleteIncident=<?= base64_encode($row ['Ref_ID']) ?>" onclick="return confirm('Are you sure to delete this item?')"><i class="fa fa-trash-o"></i>&nbsp;</a>
+                                        <a href="" data-toggle="modal" data-target="#Incident_Update-<?= $row['Ref_ID'] ?>"><i class="fa fa-pencil" data-toggle="tooltip" title="Edit"></i>&nbsp;</a>
+                                        <a href="delete.php?DeleteIncident=<?= base64_encode($row ['Ref_ID']) ?>" onclick="return confirm('Are you sure to delete this item?')"><i class="fa fa-trash-o" data-toggle="tooltip" title="Delete"></i>&nbsp;</a>
                                     </td>
                                 </tr>
                                     <?php
@@ -97,4 +97,66 @@ while ($row = mysqli_fetch_assoc($result)){
 <?php
 }
 ?>
+
+    <!-- Modal for Edit or Update -->
+<?php
+$result = mysqli_query($con, "SELECT * FROM testing");
+while ($row = mysqli_fetch_assoc($result)){
+    $Ref_ID = $row['Ref_ID'];
+    $Incident_Details = mysqli_query($con, "SELECT * FROM testing WHERE Ref_ID ='$Ref_ID'");
+    $Incident_Details_Row = mysqli_fetch_assoc($Incident_Details);
+    ?>
+    <!-- Modal -->
+    <div class="modal fade" id="Incident_Update-<?= $row['Ref_ID'] ?>" tabindex="-1" role="dialog" aria-labelledby="modal-info-label">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header state modal-info">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="modal-info-label"><i class="fa fa-edit"></i>Update Incident</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="panel">
+                        <div class="panel-content">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form class="form-horizontal" method="post" action="">
+                                        <div class="form-group">
+                                            <label for="default-datepicker" class="col-sm-4 control-label ">Date</label>
+                                            <div class="col-sm-5">
+                                                <div class="input-group">
+                                                    <span class="input-group-addon x-primary"><i class="fa fa-calendar"></i></span>
+                                                    <input type="date" name="Date" class="form-control" id="default-datepicker" value="<?= $Incident_Details_Row['Date'] ?>" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="email2" class="col-sm-4 control-label">Incident No</label>
+                                            <div class="col-sm-8">
+                                                <input type="text" name="Incident" class="form-control" id="email2" placeholder="Incident Number" value="<?= $Incident_Details_Row['Incident'] ?>" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="password2" class="col-sm-4 control-label">Remarks</label>
+                                            <div class="col-sm-8">
+                                                <input type="text" name="Remarks" class="form-control" id="password2" placeholder="Remarks" value="<?= $Incident_Details_Row['Remarks'] ?>" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-2 col-sm-10">
+                                                <button type="submit" name="Incident_Update" class="btn btn-primary"><i class="fa fa-save"></i> Update</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}
+?>
+
 <?php require_once 'footer.php'; ?>
--- header.php
+++ header.php
@@ -67,95 +67,15 @@ $page = end($page);
         <div class="rightside-header">
             <div class="header-middle"></div>
 
-            <!--NOCITE HEADERBOX-->
-            <div class="header-section hidden-xs" id="notice-headerbox">
-
-                <!--alerts notices-->
-                <div class="notice" id="alerts-notice">
-                    <i class="fa fa-bell-o" aria-hidden="true"><span class="badge badge-xs badge-top-right x-danger">7</span></i>
-
-                    <div class="dropdown-box basic">
-                        <div class="drop-header">
-                            <h3><i class="fa fa-bell-o" aria-hidden="true"></i> Notifications</h3>
-                            <span class="badge x-danger b-rounded">7</span>
-
-                        </div>
-                        <div class="drop-content">
-                            <div class="widget-list list-left-element list-sm">
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <div class="left-element"><i class="fa fa-warning color-warning"></i></div>
-                                            <div class="text">
-                                                <span class="title">8 Bugs</span>
-                                                <span class="subtitle">reported today</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <div class="left-element"><i class="fa fa-flag color-danger"></i></div>
-                                            <div class="text">
-                                                <span class="title">Error</span>
-                                                <span class="subtitle">sevidor C down</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <div class="left-element"><i class="fa fa-cog color-dark"></i></div>
-                                            <div class="text">
-                                                <span class="title">New Configuration</span>
-                                                <span class="subtitle"></span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <div class="left-element"><i class="fa fa-tasks color-success"></i></div>
-                                            <div class="text">
-                                                <span class="title">14 Task</span>
-                                                <span class="subtitle">completed</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <div class="left-element"><i class="fa fa-envelope color-primary"></i></div>
-                                            <div class="text">
-                                                <span class="title">21 Menssage</span>
-                                                <span class="subtitle"> (see more)</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="drop-footer">
-                            <a>See all notifications</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="header-separator"></div>
-            </div>
             <!--USER HEADERBOX -->
             <div class="header-section" id="user-headerbox">
                 <div class="user-header-wrap">
                     <div class="user-photo">
-                        <img alt="profile photo" src="images/avatar/avatar_user.jpg" />
+                        <img alt="profile photo" src="images/engineer.png" />
                     </div>
                     <div class="user-info">
-                        <span class="user-name">Jane Doe</span>
+                        <span class="user-name">Service Engineer</span>
                         <span class="user-profile">Admin</span>
-                    </div>
-                    <i class="fa fa-plus icon-open" aria-hidden="true"></i>
-                    <i class="fa fa-minus icon-close" aria-hidden="true"></i>
-                </div>
-                <div class="user-options dropdown-box">
-                    <div class="drop-content basic">
-                        <ul>
-                            <li> <a href="pages_user-profile.php"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
-                        </ul>
                     </div>
                 </div>
             </div>
@@ -187,9 +107,6 @@ $page = end($page);
                         <ul class="nav nav-left-lines" id="main-nav">
                             <!--HOME-->
                             <li class="<?= $page == 'index.php' ? 'active-item':'' ?>"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i><span>Dashboard</span></a></li>
-                            <li class="<?= $page == 'online-ups.php' ? 'active-item':'' ?>"><a href="online-ups.php"><i class="fa fa-home" aria-hidden="true"></i><span>Online UPS</span></a></li>
-                            <li class="<?= $page == 'add-asset.php' ? 'active-item':'' ?>"><a href="add-asset.php"><i class="fa fa-plus" aria-hidden="true"></i><span>Add Asset</span></a></li>
-                            <li class="<?= $page == 'incident.php' ? 'active-item':'' ?>"><a href="incident.php"><i class="fa fa-list" aria-hidden="true"></i><span>Incident List</span></a></li>
                             <li class="has-child-item close-item <?= $page == 'all-incidents.php' ? 'open-item':'' ?><?= $page == 'new-incident.php' ? 'open-item':'' ?>">
                                 <a><i class="fa fa-list" aria-hidden="true"></i><span>Incidents</span></a>
                                 <ul class="nav child-nav level-1">
