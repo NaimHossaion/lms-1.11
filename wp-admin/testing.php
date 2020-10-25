61047cde106b527e65f898ee83620909a7ac0789
Khandaker Naim Hossaion <46759442+NaimHossaion@users.noreply.github.com>
2020-10-25 11:31:07 +0600

    commit 1 from office 25-oct-2020

--- wp-admin/testing.php
+++ wp-admin/testing.php
@@ -6,5 +6,22 @@ echo "hello"; echo '<br>';
 echo $Ref_ID2; echo '<br>';
 
 ?>
+
+<?php
+
+// Declaring a variable to hold the IP
+// address getHostName() gets the name
+// of the local machine getHostByName()
+// gets the corresponding IP
+$localIP = getHostByName(getHostName());
+
+// Displaying the address
+echo $localIP;
+
+?>
+
+<?php
+echo getHostByName(php_uname('n'));
+?>
 <br>
 <button class="btn" onclick="document.location='index.php'">Cancel</button>
